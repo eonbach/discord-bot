0e6f325d7cbcedf2ed527a4030b6613919b44bae
DeMorMr <158618031+DeMorMr@users.noreply.github.com>
2024-02-04 15:14:10 +0300

    Update main.py > system_channels_lisichka1236
    Полу рабочий код создания каналов > траблы с правами, если что проблема в настройки прав каналу новости нормально ибо это ломает всё остальное, всё потому что в дискорде.py нету add_permission
    
    + Ивент ошибок
    + Команда удаление категории с каналами

--- main.py
+++ main.py
@@ -78,41 +78,64 @@ bot = MyBot(intents=intents, command_prefix=command_prefix)
 
 @bot.command() # основная структура команды
 async def createDSJ(ctx,*,text): # функция и перемены, * отвечает за пробелы в text
- if ctx.author.id == 751688827373879404
+ #if ctx.author.id == 751688827373879404:
     await ctx.reply(f'Команда {text} по вашему запросу созданна!') # Отправление сообщения при использовании команды
-    channel = await ctx.guild.create_category(f'Группа {text}') # создание категории по запросу
+    category = await ctx.guild.create_category(f'Группа {text}') # создание категории по запросу
+
     # Каналы в этой категории:
-    news = await channel.create_text_channel(f'Новости') 
-    await channel.create_text_channel(f'чат')
-    await channel.create_text_channel(f'ресуры')
+    news = await category.create_text_channel(f'Новости') 
+    await category.create_text_channel(f'чат')
+    await category.create_text_channel(f'ресуры')
     # Войс каналы:
-    await channel.create_voice_channel(f'кодинг')
-    await channel.create_voice_channel(f'приятные беседы')
+    await category.create_voice_channel(f'кодинг')
+    await category.create_voice_channel(f'приятные беседы')
     
     guild = ctx.guild # сервер на котором находится пользователь
     nrole = await guild.create_role(name=f"{text}") # создание роли
     await ctx.author.add_roles(nrole) # выдача роли пользователю который юзнул команду
+
+    nroleFORpeople = await guild.create_role(name=f"Участник: {text}") # создание роли
+ 
     
-    everyone_role = guild.default_role # опознаёт @everyone 
-    permissioEVE = discord.PermissionOverwrite( #запрещает @everyone смотреть
-                read_messages=False,  # запретить читать сообщения
-            )
-    await channel.set_permissions(everyone_role, overwrite=permissioEVE) # задать права категории
-
-    permissions = discord.PermissionOverwrite( #  права категории
-      read_messages=True,
-      send_messages=True
+    
+    permCategory = discord.PermissionOverwrite( # права @everyone
+      read_messages=False  # запретить читать сообщения
     )
+    await category.set_permissions(guild.default_role, overwrite=permCategory) # задать права категории
 
-    permissioEVE = discord.PermissionOverwrite( # ПРАВА КАНАЛА НОВОСТИ
-       send_messages=False  # запретить отправлять сообщения
+
+    permNews = discord.PermissionOverwrite( # ПРАВА КАНАЛА НОВОСТИ
+     send_messages=False  # запретить отправлять сообщения
     )
-    await news.set_permissions(everyone_role, overwrite=permissioEVE) # задать права категории
+    await news.add_permissions(permNews) # задать каналу новости в категории
 
 
-     user = guild.get_member(ctx.author.id)
-     if user:
-        await channel.set_permissions(user, overwrite=permissions)
-      pass
+
+    # Доступ к категории специально
+    permissionsCAT = discord.PermissionOverwrite(
+     read_messages = True
+    )
+    await category.set_permissions(nroleFORpeople, overwrite=permissionsCAT)
+
+
+    user = guild.get_member(ctx.author.id)
+    if user:
+        permUser = discord.PermissionOverwrite( # доступ юзеру
+          read_messages=True,
+          send_messages=True
+         )
+        await category.set_permissions(user, overwrite=permUser)
+       # pass 
+
+
+@bot.command()
+async def deleteDSJ(ctx): # удаляет категорию с каналами
+ for channel in ctx.channel.category.channels: 
+  await channel.delete() 
+ await category.delete()
+
+@bot.event # ивент ошибок
+async def on_command_error(ctx,error):
+     await ctx.send(f'```{error}```')
 
 bot.run(TOKEN)
